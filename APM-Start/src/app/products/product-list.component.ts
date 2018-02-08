cb8e44168a499c689f2642e87f69cf9629c3c938
Christer Carlsson <ccarlsson@outlook.com>
2018-02-08 21:50:56 +0100

    Add Stars

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -68,6 +68,9 @@ export class ProductListComponent implements OnInit {
         return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 
     ngOnInit(): void {
         console.log('In OnInit');
