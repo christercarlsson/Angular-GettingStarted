f5ea245c006de2608904be6272f40310c2f89b6d
Christer Carlsson <ccarlsson@outlook.com>
2018-02-11 20:03:04 +0100

    Refactoring

--- APM-Start/src/app/products/product-guard.service.ts
+++ APM-Start/src/app/products/product-guard.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 
 @Injectable()
-export class ProductGuardService implements CanActivate{
+export class ProductGuardService implements CanActivate {
 
-  canActivate(route: ActivatedRouteSnapshot): boolean  {
-    let id = +route.url[1].path;
-    if(isNaN(id) || id < 1) {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const id = +route.url[1].path;
+    if (isNaN(id) || id < 1) {
       // alert(`Invalid product id: ` + `(` + id + `)`);
       this._router.navigate(['/products']);
       return false;
     }
-    return  true;
+    return true;
   }
   constructor(private _router: Router) { }
 
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http'
-import { Observable } from 'rxjs/Observable'
-import 'rxjs/add/observable/throw'
-import 'rxjs/add/operator/catch'
-import 'rxjs/add/operator/do'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
-import { IProduct } from "./product";
+import { IProduct } from './product';
 
 @Injectable()
 export class ProductService {
@@ -17,7 +17,6 @@ export class ProductService {
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
-    
     private handleError(err: HttpErrorResponse) {
         console.log(err.message);
         return Observable.throw(err.message);
